3bd9be2f96b29c319e986a5e0de183f6a58fce10
Michal Wronski <michal.robert.wronski@gmail.com>
2017-10-30 11:51:45 +0100

    Minor change.

--- src/main/kotlin/com/herolynx/service/conversions/ObjectMapperProvider.kt
+++ src/main/kotlin/com/herolynx/service/conversions/ObjectMapperProvider.kt
@@ -9,7 +9,7 @@ import javax.ws.rs.ext.Provider
 @Provider
 class ObjectMapperProvider : ContextResolver<ObjectMapper> {
 
-    val objectMapper = ObjectMapper()
+    private val objectMapper = ObjectMapper()
             .enable(SerializationFeature.INDENT_OUTPUT) // pretty-print
             .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) // use iso-8601 for dates
             .registerModule(KotlinModule()) // this doesn't seem to be required to serialize our data class, but it is required to deserialize
